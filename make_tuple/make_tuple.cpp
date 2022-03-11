81cd5a5bcbadf9030c024397e270f7aed6acb4f9
Jan-idaoba <15914356786@163.com>
2022-03-11 14:37:45 +0800

    [Modern C++] 描述：学习现代C++。

---
+++ make_tuple/make_tuple.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+tuple<int, double, string> func() 
+{
+    return make_tuple(1, 1.2, "test");
+}
+
+int main()
+{
+   tuple<int,double,string> a = func();
+   auto [x, y, z] = func();
+
+   cout << x << "," << y << "," << z << endl;
+   system("pause");
+}
+
