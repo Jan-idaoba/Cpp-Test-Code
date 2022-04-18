3e660e8d9f5c75195ae0c702c95e1bf335cb63fd
Jan-idaoba <15914356786@163.com>
2022-04-18 14:04:09 +0800

    [Cpp-Test-Code] 描述：习题。

---
+++ 第二章习题/第二章习题.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+template<typename Key,typename Value,typename F>
+void update(map<Key, Value>& m, F foo)
+{
+	for (auto&& [key, value] : m)
+		value = foo(key);
+}
+
+template<typename ...T>
+auto average(T ...t)
+{
+	return (t + ...) / sizeof...(t);
+}
+
+int main()
+{
+	map<string, long long int>m{ 
+		{"a",1},
+		{"b",2}, 
+		{"c",3}
+	};
+
+	update(m, [](string key)->long long int
+		{
+			return hash<string>{}(key);
+		});
+
+	for (auto&& [key,value] : m)
+	{
+		cout << key << ":" << value << endl;
+	}
+
+	cout << average(1, 2, 3, 4, 5, 6, 7, 8, 9) << endl;
+	system("puase");
+}
